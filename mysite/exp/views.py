d3bcba87f675edd04e02f2196bd8f56618df1bcc
Kangshiqi0108 <3509148389@qq.com>
2023-03-29 16:45:26 +0800

    views update

--- mysite/exp/views.py
+++ mysite/exp/views.py
@@ -25,5 +25,22 @@ class relrDetail(APIView):
             return  relatedresources.objects.get(pk=pkey)
         except relatedresources.DoesNotExist:
             return Http404
-            
+    
+    def get(self, request, pk, format=None):
+        det = self.get_object(pk)
+        serializer = relrSeria(det)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        det = self.get_object(pk)
+        serializer = relrSeria(det, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        det = self.get_object(pk)
+        det.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
             
