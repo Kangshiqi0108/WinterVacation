3c9f63ab26ebf66f885160cbed26785e890283fc
Kangshiqi0108 <95609885+Kangshiqi0108@users.noreply.github.com>
2023-06-04 18:29:07 +0800

    Update settings.py
    fixed some bugs

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'exp_db',
         'USER': 'root',
-        'PASSWORD': '030108',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': 3306,
     }
